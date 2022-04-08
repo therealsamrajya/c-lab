e6a4df280bb936af6fcd23240131c6571d43716a
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2022-04-08 11:47:13 +0545

    Add files via upload

---
+++ fnh.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+main()
+{
+	char ch='c';
+	int i=70;
+	float f=6.14;
+	char *str="patan";
+	ofstream fout("Text.txt");
+	fout<<ch<<" "<<i<<" "<<f<<" "<<str;
+	cout<<"data written in file";
+	ifstream fin("Text.txt");
+	fin>>ch>>i>>f>>str;
+
+}
